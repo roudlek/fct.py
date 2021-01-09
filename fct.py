a3413ce3d884160118388b828c236df4cdd3d5b1
roudlek <57861799+roudlek@users.noreply.github.com>
2021-01-09 23:08:04 +0100

    Update fct.py

--- fct.py
+++ fct.py
@@ -1,5 +1,5 @@
 f = 1
 nb = int(input("entrer un nombre entier positif : "))
-for i in range (1 , nb + 1):
-    f = f * i
+for i in range (1 , nb + 1): # you dont need to declare i, if you're making a loop
+    f = f * i                # ( l'affectation a chaque fois la boucle continue(f))1 = 1 * 1 , 1 = 1 * 2 , 2 = 2 * 3 , 3 = 3 * 4 ...
 print("factorielle de",nb,"est :",f)
